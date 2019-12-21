47868537ccaae689b77e43b4983102a45939d9e0
devdattakhoche <49261633+devdattakhoche@users.noreply.github.com>
2019-12-21 15:12:06 +0530

    Add files via upload

--- Spring_Workspaces/Microservice-1/src/main/java/com/Microservice1/MicroserviceController.java
+++ Spring_Workspaces/Microservice-1/src/main/java/com/Microservice1/MicroserviceController.java
@@ -3,15 +3,21 @@ package com.Microservice1;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @RestController
+
 @RequestMapping("/Microservice-1")
 public class MicroserviceController {
+
+		
 
 	@GetMapping("/Service-1")
 	public String FirstService() throws InterruptedException
 	{
-		Thread.sleep(10000);
+		Thread.sleep(100000);
 		return "Microservice - V3";
 	}
 	
